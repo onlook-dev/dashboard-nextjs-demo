55d676f5b0e04067a3bc0f748eeec47f69983760
Kiet Ho <hoakiet98@gmail.com>
2024-03-01 17:47:56 +0000

    Adding onlook comments to multiple files

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -51,17 +51,17 @@ export default function Navbar({ user }: { user: any }) {
                 </div>
                 <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                   {navigation.map((item) => (
-<a
-  key={item.name}
-  href={item.href}
-  className={classNames(
-    pathname === item.href
-      ? 'border-slate-500 text-gray-900'
-      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-    'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium text-base'
-  )}
-  aria-current={pathname === item.href ? 'page' : undefined}
->
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? 'border-slate-500 text-gray-900'
+                          : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium text-base'
+                      )}
+                      aria-current={pathname === item.href ? 'page' : undefined}
+                    >
                       {item.name}
                     </a>
                   ))}
