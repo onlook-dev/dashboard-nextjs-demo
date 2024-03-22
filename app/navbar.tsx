4aba60f3515af7bc411bfaf1d2ec2ff6b12b43b5
Kiet Ho <hoakiet98@gmail.com>
2024-03-22 14:39:59 +0000

    Updated site using Onlook

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar({ user }: { user: any }) {
   const pathname = usePathname();
 
   return (
-    <Disclosure as="nav" className="bg-gray-200 shadow-sm" style={{backgroundColor:'#0055ff'}} />
+    <Disclosure as="nav" className="bg-gray-200 shadow-sm bg-blue-500" style={{backgroundColor:'#0055ff'}} />
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
