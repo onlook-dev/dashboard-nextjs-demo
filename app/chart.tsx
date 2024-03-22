20a5c348d276cbedae7f9a1de4ab3aaa0fb84fa5
Kiet Ho <hoakiet98@gmail.com>
2024-03-22 02:46:26 +0000

    Updated site using Onlook

--- app/chart.tsx
+++ app/chart.tsx
@@ -22,8 +22,8 @@ const data = [
 
 export default function Example() {
   return (
-    <Card className="mt-8">
-      <Title>Performance</Title>
+    <div className='bg-blue-500'>
+      <div className='bg-blue-500'>
       <Text>Comparison between Sales and Profit</Text>
       <AreaChart
         className="mt-4 h-80"
