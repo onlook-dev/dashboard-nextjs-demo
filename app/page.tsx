d7ab3e83f1e78a44d37a40dab4e7a1869378e53c
Daniel Farrell <danielfarrell.r@gmail.com>
2024-04-22 23:34:25 +0000

    Updated site using Onlook

--- app/page.tsx
+++ app/page.tsx
@@ -49,14 +49,14 @@ export default function PlaygroundPage() {
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
         {data.map((item) => (
-          <Card key={item.category}>
-            <Title className='text-[22px] text-[#0b66f9]'>{item.category}</Title>
+          <Card key={item.category} className='p-3'>
+            <Title className="text-[#2478ff]">{item.category}</Title>
             <Flex
               justifyContent="start"
               alignItems="baseline"
               className="space-x-2"
             >
-              <Metric className="text-[#124faf]">{item.stat}</Metric>
+              <Metric className="text-[#1d72fc]">{item.stat}</Metric>
               <Text>Total views</Text>
             </Flex>
             <Flex className="mt-6">
