9f31452c65ff4ad43b3699f92ba9789a04ed83c9
Daniel Farrell <danielfarrell.r@gmail.com>
2024-05-03 14:27:28 +0000

    Updated site using Onlook

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar({ user }: { user: any }) {
   const pathname = usePathname();
 
   return (
-    <Disclosure as="nav" className="bg-gray-200 shadow-sm">
+    <Disclosure as="nav" className="bg-blue-600 shadow-sm">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
